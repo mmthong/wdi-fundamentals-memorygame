4c39fda6265093aeccaa51f86baca0e932b83d10
Mabel Thong <mmthong@gmail.com>
2017-06-25 19:30:53 -0700

    Added card objects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+  	rank: "queen",
+  	suit: "hearts",
+  	cardImage: "images/queen-of-hearts.png"
+  },
+  {
+  	rank: "queen",
+  	suit: "diamonds",
+  	cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+  	rank: "king",
+  	suit: "hearts",
+  	cardImage: "images/king-of-hearts.png"
+  },
+  {
+  	rank: "king",
+  	suit: "diamonds",
+  	cardImage: "images/king-of-diamonds.png"
+  }
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -12,8 +34,10 @@ var checkForMatch = function() {
 
 var flipCard = function(cardId) {
   var card = cards[cardId];
-  cardsInPlay.push(card);
-  console.log("User flipped " + card);
+  cardsInPlay.push(card.rank);
+  console.log("User flipped " + card.rank);
+  console.log(card.cardImage);
+  console.log(card.suit);
 
   if (cardsInPlay.length === 2) {
   	checkForMatch();
