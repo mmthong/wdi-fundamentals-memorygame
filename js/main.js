b6c4354b7d1919a1320a4d57a08b93f6528500c7
Mabel Thong <mmthong@gmail.com>
2017-06-25 22:17:03 -0700

    Added events

--- js/main.js
+++ js/main.js
@@ -32,19 +32,27 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+  var cardId = this.getAttribute("data-id");
   var card = cards[cardId];
   cardsInPlay.push(card.rank);
   console.log("User flipped " + card.rank);
-  console.log(card.cardImage);
-  console.log(card.suit);
+  this.setAttribute("src", cards[cardId].cardImage);
 
   if (cardsInPlay.length === 2) {
   	checkForMatch();
   }
-
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (i =0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
 
